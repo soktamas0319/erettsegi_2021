7ca95b1ca4dae2c9cee6f21215c3972c8f821303
Sok Tamás <soktamas@yahoo.com>
2021-07-19 22:50:58 +0200

    Arraylist létrehozva

--- karacsonyCLI/src/com/company/Main.java
+++ karacsonyCLI/src/com/company/Main.java
@@ -3,12 +3,14 @@ package com.company;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             RandomAccessFile raf = new RandomAccessFile("diszek.txt", "r");
+            ArrayList<NapiBevetel> napiBevetelek = new ArrayList<>();
             try {
                 for (String sor  = raf.readLine(); sor != null ; sor = raf.readLine()) {
                     System.out.println(sor);
