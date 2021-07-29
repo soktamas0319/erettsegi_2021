844f4094410f02799499c8c7f2630822cea68822
Sok Tamás <soktamas@yahoo.com>
2021-07-30 00:32:11 +0200

    Cli feladatok kész!

--- karacsonyCLI/src/com/company/Main.java
+++ karacsonyCLI/src/com/company/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             RandomAccessFile raf = new RandomAccessFile("diszek.txt", "r");
+            RandomAccessFile raf2 = new RandomAccessFile("bevetel.txt", "rw");
             ArrayList<NapiBevetel> napiTermelesEsEladas = new ArrayList<>();
             try {
                 //arrayList feltöltés, kiírás
@@ -52,13 +53,14 @@ public class Main {
                 int harangKeszlet = 0;
                 int angyalkaKeszlet = 0;
                 int fenyofaKeszlet = 0;
-                for ( i = 0; i <= napSzama-1; i++) {
+                for (i = 0; i <= napSzama - 1; i++) {
                     harangKeszlet += napiTermelesEsEladas.get(i).getHarangKesz() + napiTermelesEsEladas.get(i).getHarangEladott();
                     angyalkaKeszlet += napiTermelesEsEladas.get(i).getAngyalkaKesz() + napiTermelesEsEladas.get(i).getAngyalkaEladott();
                     fenyofaKeszlet += napiTermelesEsEladas.get(i).getFenyofaKesz() + napiTermelesEsEladas.get(i).getFenyofaEladott();
                 }
+                System.out.println("6. feladat");
                 System.out.println("Harang: " + harangKeszlet + System.lineSeparator()
-                        + "Angyalka: " + angyalkaKeszlet +System.lineSeparator()
+                        + "Angyalka: " + angyalkaKeszlet + System.lineSeparator()
                         + "Fenyőfa:" + fenyofaKeszlet);
 
                 //legtöbbet eladott dísz
@@ -66,14 +68,37 @@ public class Main {
                 int osszesEladottAngyalka = 0;
                 int osszesEladottFenyofa = 0;
                 for (int j = 0; j < napiTermelesEsEladas.size(); j++) {
-                    osszesEladottHarang += napiTermelesEsEladas.get(j).getHarangEladott();
-                    osszesEladottAngyalka += napiTermelesEsEladas.get(j).getAngyalkaEladott();
-                    osszesEladottFenyofa += napiTermelesEsEladas.get(j).getFenyofaEladott();
+                    osszesEladottHarang -= napiTermelesEsEladas.get(j).getHarangEladott();
+                    osszesEladottAngyalka -= napiTermelesEsEladas.get(j).getAngyalkaEladott();
+                    osszesEladottFenyofa -= napiTermelesEsEladas.get(j).getFenyofaEladott();
                 }
-                System.out.println("Eladott harang: " + osszesEladottHarang);
-                System.out.println("Eladott angyalka: " + osszesEladottAngyalka);
-                System.out.println("Eladott fenyofa: " + osszesEladottFenyofa);
 
+                int max = Integer.max(Integer.max(osszesEladottAngyalka, osszesEladottFenyofa), osszesEladottHarang);
+                System.out.println();
+                System.out.println("7. feladat:");
+                System.out.println("Legtöbbet eladott dísz:");
+                if (max == osszesEladottHarang){
+                    System.out.println("Harang: " + max);
+                }
+                if (max == osszesEladottAngyalka){
+                    System.out.println("Angyalka: " + max);
+                }
+                if (max == osszesEladottFenyofa){
+                    System.out.println("Fenyofa: " + max);
+                }
+
+                //8. feladat
+                //10.000 Ft-os, vagy több napi bevételek fájlba írása
+                int szamlalo = 0;
+                for (NapiBevetel nb: napiTermelesEsEladas) {
+                    if (nb.napiBevetel() >= 10000){
+                        raf2.writeBytes( Integer.toString(nb.getNap())
+                                + ":" + Integer.toString(nb.napiBevetel())
+                                + System.lineSeparator());
+                        szamlalo++;
+                    }
+                }
+                raf2.writeBytes(Integer.toString(szamlalo) + " napon volt legalább 10.000 Ft a bevétel.");
 
             } catch (IOException e) {
                 e.printStackTrace();
